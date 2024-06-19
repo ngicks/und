7023c73fcedcae8014dd8007bcbd230af3cc6824
ngicks <yknt.bsl@gmail.com>
2024-06-19 16:59:18 +0000

    👷: add comparison bench

---
+++ internal/bench/beanch_test.go
@@ -0,0 +1,108 @@
+package bench
+
+import (
+	"encoding/json"
+	"testing"
+
+	jsonv2 "github.com/go-json-experiment/json"
+)
+
+var inputs = []string{
+	`{"Pad2":123}`,
+	`{"U":null}`,
+	`{"Pad1":445,"U":123}`,
+}
+
+var expected = []int{
+	0,   // undefined
+	1,   // null
+	123, // value
+}
+
+type sampleMapV1 struct {
+	Pad1 int         `json:",omitempty"`
+	U    undMap[int] `json:",omitempty"`
+	Pad2 int         `json:",omitempty"`
+}
+
+type sampleMapV2 struct {
+	Pad1 int         `json:",omitzero"`
+	U    undMap[int] `json:",omitzero"`
+	Pad2 int         `json:",omitzero"`
+}
+
+type sampleSliceV1 struct {
+	Pad1 int           `json:",omitempty"`
+	U    undSlice[int] `json:",omitempty"`
+	Pad2 int           `json:",omitempty"`
+}
+
+type sampleSliceV2 struct {
+	Pad1 int           `json:",omitzero"`
+	U    undSlice[int] `json:",omitzero"`
+	Pad2 int           `json:",omitzero"`
+}
+
+func BenchmarkSerdeMapV1(b *testing.B) {
+	for range b.N {
+		for _, input := range inputs {
+			var s sampleMapV1
+			err := json.Unmarshal([]byte(input), &s)
+			if err != nil {
+				panic(err)
+			}
+			_, err = json.Marshal(s)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func BenchmarkSerdeSliceV1(b *testing.B) {
+	for range b.N {
+		for _, input := range inputs {
+			var s sampleSliceV1
+			err := json.Unmarshal([]byte(input), &s)
+			if err != nil {
+				panic(err)
+			}
+			_, err = json.Marshal(s)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func BenchmarkSerdeMapV2(b *testing.B) {
+	for range b.N {
+		for _, input := range inputs {
+			var s sampleMapV2
+			err := jsonv2.Unmarshal([]byte(input), &s)
+			if err != nil {
+				panic(err)
+			}
+			_, err = jsonv2.Marshal(s)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func BenchmarkSerdeSliceV2(b *testing.B) {
+	for range b.N {
+		for _, input := range inputs {
+			var s sampleSliceV2
+			err := jsonv2.Unmarshal([]byte(input), &s)
+			if err != nil {
+				panic(err)
+			}
+			_, err = jsonv2.Marshal(s)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
---
+++ internal/bench/slice.go
@@ -0,0 +1,108 @@
+package bench
+
+import (
+	"encoding/json"
+
+	jsonv2 "github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+	"github.com/ngicks/und/v2"
+)
+
+type undSlice[T any] []und.Option[T]
+
+func (u undSlice[T]) IsZero() bool {
+	return u.IsUndefined()
+}
+
+func (u undSlice[T]) IsDefined() bool {
+	return len(u) > 0 && u[0].IsSome()
+}
+
+func (u undSlice[T]) IsNull() bool {
+	return len(u) > 0 && u[0].IsNone()
+}
+
+func (u undSlice[T]) IsUndefined() bool {
+	return len(u) == 0
+}
+
+func (u undSlice[T]) Value() T {
+	if u.IsDefined() {
+		return u[0].Value()
+	}
+	var zero T
+	return zero
+}
+
+var _ json.Marshaler = undSlice[any]{}
+
+func (u undSlice[T]) MarshalJSON() ([]byte, error) {
+	if !u.IsDefined() {
+		return []byte(`null`), nil
+	}
+	return json.Marshal(u[0].Value())
+}
+
+var _ json.Unmarshaler = (*undSlice[any])(nil)
+
+func (u *undSlice[T]) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		if len(*u) == 0 {
+			*u = []und.Option[T]{und.None[T]()}
+		} else {
+			(*u)[0] = und.None[T]()
+		}
+		return nil
+	}
+
+	var t T
+	err := json.Unmarshal(data, &t)
+	if err != nil {
+		return err
+	}
+
+	if len(*u) == 0 {
+		*u = []und.Option[T]{und.Some(t)}
+	} else {
+		(*u)[0] = und.Some(t)
+	}
+	return nil
+}
+
+var _ jsonv2.MarshalerV2 = undSlice[any]{}
+
+func (u undSlice[T]) MarshalJSONV2(enc *jsontext.Encoder, opts jsonv2.Options) error {
+	if !u.IsDefined() {
+		return enc.WriteToken(jsontext.Null)
+	}
+	return jsonv2.MarshalEncode(enc, u.Value(), opts)
+}
+
+var _ jsonv2.UnmarshalerV2 = (*undSlice[any])(nil)
+
+func (u *undSlice[T]) UnmarshalJSONV2(dec *jsontext.Decoder, opts jsonv2.Options) error {
+	if dec.PeekKind() == 'n' {
+		err := dec.SkipValue()
+		if err != nil {
+			return err
+		}
+		if len(*u) == 0 {
+			*u = []und.Option[T]{und.None[T]()}
+		} else {
+			(*u)[0] = und.None[T]()
+		}
+		return nil
+	}
+	var t T
+	err := jsonv2.UnmarshalDecode(dec, &t, opts)
+	if err != nil {
+		return err
+	}
+
+	if len(*u) == 0 {
+		*u = []und.Option[T]{und.Some(t)}
+	} else {
+		(*u)[0] = und.Some(t)
+	}
+	return nil
+}
