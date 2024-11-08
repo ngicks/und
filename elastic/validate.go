67d88238795b9e837e9bfce9aeaf839dc4084899
ngicks <yknt.bsl@gmail.com>
2024-11-08 22:56:08 +0000

    ✨: allow validate to skip some indices
    Since T could be *U (where U is any), calling UndValidate on nil value might panic.

--- elastic/validate.go
+++ elastic/validate.go
@@ -1,15 +1,21 @@
 package elastic
 
 import (
+	"slices"
+	"strconv"
+
 	"github.com/ngicks/und/option"
 	"github.com/ngicks/und/validate"
 )
 
-func UndValidate[T validate.UndValidator](e Elastic[T]) error {
+func UndValidate[T validate.UndValidator](e Elastic[T], skip ...int) error {
 	return option.MapOr(e.Unwrap().Unwrap(), nil, func(opt option.Option[option.Options[T]]) error {
-		for _, opt := range opt.Value() {
+		for i, opt := range opt.Value() {
+			if slices.Contains(skip, i) {
+				continue
+			}
 			if err := option.MapOr(opt, nil, func(t T) error { return t.UndValidate() }); err != nil {
-				return err
+				return validate.AppendValidationErrorIndex(err, strconv.FormatInt(int64(i), 10))
 			}
 		}
 		return nil
