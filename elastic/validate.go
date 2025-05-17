778f6a4a00a13a24a8482b4279c40972ae89b78e
ngicks <yknt.bsl@gmail.com>
2025-05-18 02:34:51 +0900

    ✨: [BREAKING] use skipIf callback function instead of receiving list of indices.

--- elastic/validate.go
+++ elastic/validate.go
@@ -1,17 +1,16 @@
 package elastic
 
 import (
-	"slices"
 	"strconv"
 
 	"github.com/ngicks/und/option"
 	"github.com/ngicks/und/validate"
 )
 
-func UndValidate[T validate.UndValidator](e Elastic[T], skip ...int) error {
+func UndValidate[T validate.UndValidator](e Elastic[T], skipIf func(int, option.Option[T]) bool) error {
 	return option.MapOr(e.Unwrap().Unwrap(), nil, func(opt option.Option[option.Options[T]]) error {
 		for i, opt := range opt.Value() {
-			if slices.Contains(skip, i) {
+			if skipIf != nil && skipIf(i, opt) {
 				continue
 			}
 			if err := option.MapOr(opt, nil, func(t T) error { return t.UndValidate() }); err != nil {
